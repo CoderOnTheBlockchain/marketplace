b97d80c5263cec21a51ae57f49c861fb34ef75ce
Raldo CHEA <raldo.chea@gmail.com>
2022-05-07 12:12:59 +0200

    Truffle config and default config params

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,44 @@
 const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require('dotenv').config();
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      host: "127.0.0.1",
+      port: `${process.env.LOCAL_PORT}`,
+      network_id: "*"
+    },
+    kovan: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic:       {phrase: `${process.env.MNEMONIC}`},
+          providerOrUrl:  `https://kovan.infura.io/v3/${process.env.INFURA_ID}`,
+        }),
+      network_id: '42'
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic:       {phrase: `${process.env.MNEMONIC}`},
+          providerOrUrl:  `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`,
+        }),
+      network_id: '3'
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic:       {phrase: `${process.env.MNEMONIC}`},
+          providerOrUrl:  `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}`,
+        }),
+      network_id: '4'
     }
-  }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.13"
+    }
+  },
+  mocha: {}
 };
