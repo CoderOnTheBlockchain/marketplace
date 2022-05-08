99a20ce00bd5521535429a5f25726418a8882b96
Raldo CHEA <raldo.chea@gmail.com>
2022-05-09 01:36:11 +0200

    Inverse file name home & home2

--- client/src/components/pages/home.js
+++ client/src/components/pages/home.js
@@ -1,94 +1,182 @@
 import React from 'react';
-import SliderCarousel from '../components/SliderCarousel';
+import Particle from '../components/Particle';
+import SliderMainParticle from '../components/SliderMainParticle';
 import FeatureBox from '../components/FeatureBox';
 import CarouselCollection from '../components/CarouselCollection';
-import ColumnNew from '../components/ColumnNew';
+import CarouselNew from '../components/CarouselNew';
 import AuthorList from '../components/authorList';
 import Footer from '../components/footer';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-  header#myHeader.navbar.sticky.white {
-    background: #212428;
-    border-bottom: 0;
-    box-shadow: 0 4px 20px 0 rgba(10,10,10, .8);
-  }
-  header#myHeader.navbar .search #quick_search{
-    color: #fff;
-    background: rgba(255, 255, 255, .1);
-  }
-  header#myHeader.navbar.white .btn, .navbar.white a, .navbar.sticky.white a{
-    color: #fff;
-  }
-  header#myHeader .dropdown-toggle::after{
-    color: #fff;
-  }
   header#myHeader .logo .d-block{
     display: none !important;
   }
   header#myHeader .logo .d-none{
-    display: none !important;
-  }
-  header#myHeader .logo .d-3{
     display: block !important;
   }
-  footer.footer-light .subfooter span img.d-1{
-    display: none !important;
+  .navbar .mainside a{
+    background: #8364e2;
+    &:hover{
+      box-shadow: 2px 2px 20px 0px #8364e2;
+    }
   }
-  footer.footer-light .subfooter span img.d-3{
-    display: inline-block !important;
+  .item-dropdown{
+    .dropdown{
+      a{
+        &:hover{
+          background: #8364e2;
+        }
+      }
+    }
+  }
+  .btn-main{
+    background: #8364e2;
+    &:hover{
+      box-shadow: 2px 2px 20px 0px #8364e2;
+    }
+  }
+  p.lead{
+    color: #a2a2a2;
+  }
+  .navbar .navbar-item .lines{
+    border-bottom: 2px solid #8364e2;
+  }
+  .jumbotron.no-bg{
+    height: 100vh;
+    overflow: hidden;
+    background-repeat: repeat;
+    background-size: cover;
+    background-position: bottom;
+    background-repeat: no-repeat;
+  }
+  #tsparticles{
+    top: 0;
+  }
+  .text-uppercase.color{
+    color: #8364e2;
+  }
+  .de_count h3 {
+    font-size: 36px;
+    margin-bottom: 0px;
+  }
+  .de_count h5{
+    font-size: 14px;
+    font-weight: 500;
+  }
+  h2 {
+    font-size: 30px;
+  }
+  .box-url{
+    text-align: center;
+    h4{
+      font-size: 16px;
+    }
   }
   .de_countdown{
-    right: 10px;
-    color: #fff;
+    border: solid 2px #8364e2;
   }
-  .author_list_pp{
-    margin-left:0;
+  .author_list_pp, .author_list_pp i, 
+  .nft_coll_pp i, .feature-box.style-3 i, 
+  footer.footer-light #form_subscribe #btn-subscribe i, 
+  #scroll-to-top div{
+    background: #8364e2;
   }
-  footer.footer-light .subfooter{
-    border-top: 1px solid rgba(255,255,255,.1);
+  footer.footer-light .subfooter .social-icons span i{
+    background: #403f83;
+  }
+  .author_list_pp:hover img{
+    box-shadow: 0px 0px 0px 2px #8364e2;
+  }
+  .nft__item_action span{
+    color: #8364e2;
+  }
+  .feature-box.style-3 i.wm{
+    color: rgba(131,100,226, .1);
+  }
+  @media only screen and (max-width: 1199px) {
+    .navbar{
+      
+    }
+    .navbar .menu-line, .navbar .menu-line1, .navbar .menu-line2{
+      background: #fff;
+    }
+    .item-dropdown .dropdown a{
+      color: #fff !important;
+    }
   }
 `;
 
-const homethree= () => (
+
+const homeone= () => (
   <div>
   <GlobalStyles />
-      <section className="jumbotron no-bg no-bottom">
-        <div className='container-fluid'>
-          <div className='row'>
-             <SliderCarousel/>
-          </div>
-        </div>
+      <section className="jumbotron no-bg" style={{backgroundImage: `url(${'./img/background/8.jpg'})`}}>
+       <Particle/>
+         <SliderMainParticle/>
       </section>
 
-      <section className='container'>
-        <div className='row'>
-          <div className='col-lg-12'>
-              <h2 className='style-2'>New Items</h2>
-          </div>
-        </div>
-       <ColumnNew/>
-      </section>
-
-      <section className='container no-top'>
-        <div className='row'>
-          <div className='col-lg-12'>
-              <h2 className='style-2'>Hot Collections</h2>
-          </div>
-        </div>
-        <div className='container no-top'>
-          <div className='row'>
-            <div className='col-lg-12 px-0'>
-              <CarouselCollection/>
+      <section className='container no-bottom'>
+        <div className="row">
+            <div className="col-lg-2 col-sm-4 col-6 mb30">
+                <span className="box-url">
+                    <img src="./img/wallet/1.png" alt="" className="mb20"/>
+                    <h4>Metamask</h4>
+                </span>
             </div>
-          </div>
+
+            <div className="col-lg-2 col-sm-4 col-6 mb30">
+                <span className="box-url">
+                    <img src="./img/wallet/2.png" alt="" className="mb20"/>
+                    <h4>Bitski</h4>
+                </span>
+            </div>       
+
+            <div className="col-lg-2 col-sm-4 col-6 mb30">
+                <span className="box-url">
+                    <img src="./img/wallet/3.png" alt="" className="mb20"/>
+                    <h4>Fortmatic</h4>
+                </span>
+            </div>    
+
+            <div className="col-lg-2 col-sm-4 col-6 mb30">
+                <span className="box-url">
+                    <img src="./img/wallet/4.png" alt="" className="mb20"/>
+                    <h4>WalletConnect</h4>
+                </span>
+            </div>
+
+            <div className="col-lg-2 col-sm-4 col-6 mb30">
+                <span className="box-url">
+                    <img src="./img/wallet/5.png" alt="" className="mb20"/>
+                    <h4>Coinbase Wallet</h4>
+                </span>
+            </div>
+
+            <div className="col-lg-2 col-sm-4 col-6 mb30">
+                <span className="box-url">
+                    <img src="./img/wallet/6.png" alt="" className="mb20"/>
+                    <h4>Arkane</h4>
+                </span>
+            </div>                                       
         </div>
       </section>
 
-      <section className='container no-top'>
+      <section className='container no-top no-bottom'>
         <div className='row'>
+          <div className="spacer-double"></div>
+          <div className='col-lg-12 mb-2'>
+              <h2>New Items</h2>
+          </div>
+        </div> 
+        <CarouselNew/>
+      </section>
+
+      <section className='container no-top no-bottom'>
+        <div className='row'>
+          <div className="spacer-double"></div>
           <div className='col-lg-12'>
-              <h2 className='style-2'>Top Seller</h2>
+              <h2>Top Sellers</h2>
           </div>
           <div className='col-lg-12'>
             <AuthorList/>
@@ -96,14 +184,25 @@ const homethree= () => (
         </div>
       </section>
 
+      <section className='container no-top no-bottom'>
+        <div className='row'>
+          <div className="spacer-double"></div>
+          <div className='col-lg-12 mb-2'>
+              <h2>Hot Collections</h2>
+          </div>
+            <div className='col-lg-12'>
+              <CarouselCollection/>
+            </div>
+          </div>
+      </section>
+
       <section className='container no-top'>
         <div className='row'>
-          <div className='col-lg-12'>
-              <h2 className='style-2'>Create and sell</h2>
-          </div>
-        </div>
-        <div className='container px-0'>
-          <FeatureBox/>
+            <div className="spacer-double"></div>
+            <div className='col-lg-12 mb-3'>
+              <h2>Create and Sell Now</h2>
+            </div>
+            <FeatureBox/>
         </div>
       </section>
 
@@ -111,4 +210,4 @@ const homethree= () => (
 
   </div>
 );
-export default homethree;
+export default homeone;
