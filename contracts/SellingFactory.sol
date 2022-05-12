707d5096980b3f7432ecf943a7c6e3424b9dcb1b
AdamK <83402406+amsteradams@users.noreply.github.com>
2022-05-12 13:00:50 +0200

    ajout commentaires

--- contracts/SellingFactory.sol
+++ contracts/SellingFactory.sol
@@ -1,17 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
+///@author Adam
 
 import "./AuctionOrSale.sol";
 
 contract SellingFactory{
-    address[] allSales;
-    address[] allAuctions;
+    ///@dev il faudrait créer une fonction qui nettoie les tableaux et qui se lance à chaque création de auctionOrSale
+    address[] allSales; //stockage des collections en vente directe
+    address[] allAuctions;//stockage des collections aux encheres
+
 
     event NewSale(address indexed _contract, address _seller);
     event NewAuction(address indexed _contract, address _seller);
 
     event Debug(address _addr);
-    //set _auctionTime to 0 for a sale
+    /**
+    *@dev impossible de faire 2 contrats à cause du problème d'interface
+    *@notice crée un nouveau contrat de vente ou d'auction pour un nft
+    *@param _nft est l'addresse de la collection du nft
+    *@param _nftId est l'id du nft
+    *@param _price est le prix de vente dans le cas d'une sale, et le prix de départ pour une auction
+    *@param _auctionTime est le temps que doit durer l'enchere. Si il est à zero la fonction crée une vente, sinon une auction 
+    */
     function newSaleOrAuction(address _nft, uint _nftId, uint _price, uint32 _auctionTime)external{
         if(_auctionTime == 0){
             AuctionOrSale _contract = new AuctionOrSale(_nft, _nftId, _price, 0, payable(msg.sender));
